1cad82eb59e6cee05246de3361108c2540a85456
Woody Peterson <woody.peterson@gmail.com>
2012-08-09 10:09:49 -0700

    Fix migration for heroku (doesnt support models)

--- db/migrate/20120809162722_add_cache_columns_to_orders.rb
+++ db/migrate/20120809162722_add_cache_columns_to_orders.rb
@@ -4,7 +4,5 @@ class AddCacheColumnsToOrders < ActiveRecord::Migration
     add_column :orders, :line_items_count, :integer
     add_column :orders, :total, :decimal, :precision => 8, :scale => 2
     add_column :orders, :payment_status, :string
-
-    Order.find_each(&:save)
   end
 end
