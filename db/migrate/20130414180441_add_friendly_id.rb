044964ead53337d4c6b76584fcb44028e7c04765
Woody Peterson <woody.peterson@gmail.com>
2013-04-14 15:23:02 -0700

    Attempt to get the migration to run on heroku

--- db/migrate/20130414180441_add_friendly_id.rb
+++ db/migrate/20130414180441_add_friendly_id.rb
@@ -1,4 +1,9 @@
 class AddFriendlyId < ActiveRecord::Migration
+  class Category < ActiveRecord::Base
+    extend FriendlyId
+    friendly_id :name, use: [:slugged, :scoped], scope: :ancestry
+  end
+
   def change
     add_column :categories, :slug, :string
     add_index :categories, :slug
